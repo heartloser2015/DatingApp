d6c59a6a2db2797ea0c21af4bf0725f2eb9302a2
handsome ZHANG YUXUAN 2001287H <2001287H@student.tp.edu.sg>
2021-12-19 11:06:29 +0800

    added week 7

--- DatingApp/Server/Controllers/WeatherForecastController.cs
+++ DatingApp/Server/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace DatingApp.Server.Controllers
 {
-    [Authorize]
+    //[Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
